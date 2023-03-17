99626428bb72a240f93309870830d8988a48b6de
danielgarthur <4132779+danielgarthur@users.noreply.github.com>
2023-03-17 15:33:30 -0500

    Add webkit detection and simplify mark positioning

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
 import byzhtml from './lib/byzhtml.js';
 import { defineCustomElements } from './lib/util/defineCustomElements.js';
+import { isWebkit } from './lib/util/isWebkit.js';
 
-defineCustomElements();
+if (isWebkit()) {
+  console.log('byzhtml: webkit browser detected. Using legacy positioning.');
+
+  fetch(
+    'https://cdn.jsdelivr.net/gh/danielgarthur/byzhtml@latest/dist/neanes.metadata.json',
+  )
+    .then((response) => {
+      response
+        .json()
+        .then((data) => {
+          byzhtml.fontService.loadMap(byzhtml.options.defaultFontFamily, data);
+          byzhtml.options.useLegacyPositioning = true;
+        })
+        .catch((err) => {
+          console.error('could not load font metadata: ' + err);
+        })
+        .finally(defineCustomElements);
+    })
+    .catch((err) => {
+      console.error('could not load font metadata: ' + err);
+
+      // Load the page anyway, even though the styling will be bad
+      defineCustomElements();
+    });
+} else {
+  defineCustomElements();
+}
 
 export { byzhtml as default };
