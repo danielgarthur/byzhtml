0bfda8bf65bde441fcc248566cc5488243732750
danielgarthur <4132779+danielgarthur@users.noreply.github.com>
2023-03-20 14:58:27 -0500

    Add hard-coded optional glyphs

--- dist/byzhtml.js
+++ dist/byzhtml.js
@@ -1,4 +1,4 @@
-// byzhtml v1.0.14
+// byzhtml v1.0.13
 var byzhtml = (function () {
   'use strict';
 
@@ -1291,6 +1291,49 @@ var byzhtml = (function () {
         const codepoint = Number('0x' + data.codepoint.substring(2));
         this.glyphNameToCodepointMap.set(glyph, String.fromCodePoint(codepoint));
       }
+
+      // TODO hard-coding optional glyphs
+      // This is okay for now since Neanes is the only SBMuFL font
+
+      this.glyphNameToCodepointMap.set(
+        'oligonKentimataBelow.alt01',
+        String.fromCodePoint(0xf000),
+      );
+
+      this.glyphNameToCodepointMap.set(
+        'oligonKentimataAbove.alt01',
+        String.fromCodePoint(0xf001),
+      );
+
+      this.glyphNameToCodepointMap.set(
+        'antikenoma.alt01',
+        String.fromCodePoint(0xf002),
+      );
+
+      this.glyphNameToCodepointMap.set(
+        'modeFirst.salt01',
+        String.fromCodePoint(0xf003),
+      );
+
+      this.glyphNameToCodepointMap.set(
+        'modeFourth.salt01',
+        String.fromCodePoint(0xf004),
+      );
+
+      this.glyphNameToCodepointMap.set(
+        'oligonKentimataBelow.alt02',
+        String.fromCodePoint(0xf005),
+      );
+
+      this.glyphNameToCodepointMap.set(
+        'psifiston.salt01',
+        String.fromCodePoint(0xf006),
+      );
+
+      this.glyphNameToCodepointMap.set(
+        'heteronConnecting.salt01',
+        String.fromCodePoint(0xf007),
+      );
     }
   }
 
